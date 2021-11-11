b97748648b37b3d258d29182e3de8729f254335f
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-11-11 10:14:27 +0700

    Add source code, Add Redux To Details (Advanced) ... 2:14:18

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -12,4 +12,16 @@ const listProducts = () => async (dispatch) => {
     }
 }
 
-export { listProducts }
+
+const detailsProduct = (productID) => async (dispatch) => {
+    try {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST, payload: productID });
+        const { data } = await axios.get("/api/products/" + productID);
+        dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: data });
+    }
+    catch (error) {
+        dispatch({ type: PRODUCT_DETAILS_FAIL, payload: error.message });
+    }
+}
+
+export { listProducts, detailsProduct }
