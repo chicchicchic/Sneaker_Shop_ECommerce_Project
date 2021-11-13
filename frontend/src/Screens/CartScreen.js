9f888dc75388abea06d939c18f88f89fc0ab2210
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-11-14 05:17:18 +0700

    Add source code, Shopping Cart Screen (Advanced) ... 2:50:33

--- frontend/src/Screens/CartScreen.js
+++ frontend/src/Screens/CartScreen.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import { addToCart } from "../actions/cartActions";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function CartScreen(props) {
+
+    const cart = useSelector(state => state.cart);
+
+    const { cartItems } = cart;
 
     const productId = props.match.params.id;
     const quantity = props.location.search? Number(props.location.search.split("=")[1]):1;
@@ -15,8 +19,59 @@ function CartScreen(props) {
         }
     }, [])
 
-    return <div>
-        Cart Screen
+    return <div className="cart">
+        <div className="cart-list">
+            <ul className="cart-list-container">
+                <li>
+                    <h3>
+                        Shopping Cart
+                    </h3>
+                    <div>
+                        Price
+                    </div>
+                </li>
+                {
+                    cartItems.length === 0 ? 
+                    <div>
+                        Cart is empty
+                    </div>
+                    :
+                    cartItems.map(item => 
+                        <div>
+                            <img src={item.image} alt="product" />
+                            <div className="cart-name">
+                                <div>
+                                    {item.name}
+                                </div>
+                                <div>
+                                    Quantity:
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div>
+                                {item.price}
+                            </div>
+
+
+                        </div>    
+                    )
+                }
+            </ul>
+        </div>
+        <div className="cart-action">
+            <h3>
+                Subtotal ( {cartItems.reduce((a, c) => a + c.quantity, 0)} items )
+                :
+                $ {cartItems.reduce((a, c) => a + c.price * c.quantity, 0)}
+            </h3>
+            <button className="button primary" disabled={cartItems.length === 0}>
+                Proceed to Checkout
+            </button>
+        </div>
     </div>
 }
 
