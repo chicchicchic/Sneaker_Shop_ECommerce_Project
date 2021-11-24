0227946ce53294a6b3f0b83fa056335d7ac49304
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-11-25 06:27:27 +0700

    Add source code, Sign In User (JWT Authentication) ... 3:27:34

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/sneaker'
+    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/sneaker',
+    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret'
 }
