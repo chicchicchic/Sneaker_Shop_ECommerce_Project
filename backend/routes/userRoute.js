8763e1f33011e9701d54128598b4479d145f26ec
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-11-24 12:45:34 +0700

    Add source code, Connect MongoDB (Mongoose) ... 3:21:25

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import User from '../models/userModel';
+
+const router = express.Router();
+
+router.get('/createadmin', async (req, res) => {
+    try {
+        const user = new User({
+            name: 'Khanh',
+            email: 'chicken481999@gmail.com',
+            password: '5678',
+            isAdmin: true
+        });
+        const newUser = await user.save();
+        res.send(newUser);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
+})
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,18 @@ import express from 'express';
 import data from './data';
 import dotenv from 'dotenv';
 import config from './config';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
 
 
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 
 
@@ -20,6 +27,7 @@ const app = express();
 //     res.json('abc')
 // })
 
+app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
     const productId = req.params.id;
     const product = data.products.find(x=>x._id === productId);
