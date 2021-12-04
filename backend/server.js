5572397f3f54a75d6150adb45609b29c79cbe21f
chicchicchic <64607059+chicchicchic@users.noreply.github.com>
2021-12-05 04:43:10 +0700

    Add source code, Sign In User (JWT Authentication) ... 3:46:18

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,22 @@
-// const express = require('express')
-// const data = require('./data')
-
 import express from 'express';
 import data from './data';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import config from './config';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import userRoute from './routes/userRoute';
+// import cors from 'cors';
 
-dotenv.config();
+// const express = require('express')
+// const data = require('./data')
+// const dotenv = require('dotenv')
+// const config = require('./config')
+// const mongoose = require('mongoose')
+// const bodyParser = require('body-parser')
+// const userRoute = require('./routes/userRoute')
+// const cors = require('cors');
+
+// dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
 mongoose.connect(mongodbUrl, {
@@ -23,6 +30,16 @@ const app = express();
 //     res.json('abc')
 // })
 
+// Set cors in NodeJS (Nếu lỗi Cors)
+// app.use(cors());
+
+// Set cors in NodeJS (Nếu lỗi Cors)
+// app.use(function(req, res, next) {
+    //     res.header("Access-Control-Allow-Origin", "*");
+    //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    //     next();
+    // });
+    
 app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.get("/api/products/:id", (req, res) => {
@@ -33,12 +50,9 @@ app.get("/api/products/:id", (req, res) => {
     else
         res.status(404).send({ msg: "Product Not Found." })
 });
-
-
 app.get("/api/products", (req, res) => {
 
     res.send(data.products);
 });
 
-// chỉ có pot 5000 mới run dc, sửa thành pot bất kỳ ko đúng là ko run dc
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") })
+app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
